f3b5d2c319198afe33d14ae49dfb634ab3a3eba7
Michael Haggerty <mhagger@alum.mit.edu>
2013-01-24 04:57:58 +0100

    Add license information.
    The license is inherited from git because of the derivation from git's
    contrib/hooks/post-receive-email.  But make the license explicit
    within git-multimail itself.
    
    Also update the copyright date in the main file.

--- git-multimail/git_multimail.py
+++ git-multimail/git_multimail.py
@@ -1,9 +1,24 @@
 #! /usr/bin/env python2
 
-# Copyright (c) 2012 Michael Haggerty
+# Copyright (c) 2012,2013 Michael Haggerty
 # Derived from contrib/hooks/post-receive-email, which is
 # Copyright (c) 2007 Andy Parkins
 # and also includes contributions by other authors.
+#
+# This file is part of git-multimail.
+#
+# git-multimail is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version
+# 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 """Generate notification emails for pushes to a git repository.
 
