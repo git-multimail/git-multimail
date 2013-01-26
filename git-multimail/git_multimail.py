801c9b96b068211cdaab628e7c689338e1eeb75d
Michael Haggerty <mhagger@alum.mit.edu>
2013-01-26 07:02:22 +0100

    Improve comments.
    Remove the big comment from
    ReferenceChange.generate_revision_change_summary(), which was mostly
    obsolete.  Instead, add a docstring for the Push class that explains
    how it determines new/discarded commits.

--- git-multimail/git_multimail.py
+++ git-multimail/git_multimail.py
@@ -729,77 +729,6 @@ class ReferenceChange(Change):
     def generate_revision_change_summary(self, push):
         """Generate a summary of the revisions added/removed by this change."""
 
-        # Consider this:
-        #   1 --- 2 --- O --- X --- 3 --- 4 --- N
-        #
-        # O is oldrev for refname
-        # N is newrev for refname
-        # X is a revision pointed to by some other ref, for which we may
-        #   assume that an email has already been generated.
-        # In this case we want to issue an email containing only revisions
-        # 3, 4, and N.  Given (almost) by
-        #
-        #  git rev-list N ^O --not --all
-        #
-        # The reason for the "almost", is that the "--not --all" will take
-        # precedence over the "N", and effectively will translate to
-        #
-        #  git rev-list N ^O ^X ^N
-        #
-        # So, we need to build up the list more carefully.  git rev-parse
-        # will generate a list of revs that may be fed into git rev-list.
-        # We can get it to make the "--not --all" part and then filter out
-        # the "^N" with:
-        #
-        #  git rev-parse --not --all | grep -v N
-        #
-        # Then, using the --stdin switch to git rev-list we have effectively
-        # manufactured
-        #
-        #  git rev-list N ^O ^X
-        #
-        # This leaves a problem when someone else updates the repository
-        # while this script is running.  Their new value of the ref we're
-        # working on would be included in the "--not --all" output; and as
-        # our newrev would be an ancestor of that commit, it would exclude
-        # all of our commits.  What we really want is to exclude the current
-        # value of refname from the --not list, rather than N itself.  So:
-        #
-        #  git rev-parse --not --all | grep -v $(git rev-parse $refname)
-        #
-        # Get's us to something pretty safe (apart from the small time
-        # between refname being read, and git rev-parse running - for that,
-        # I give up)
-        #
-        #
-        # Next problem, consider this:
-        #   * --- B --- * --- O (oldrev)
-        #          \
-        #           * --- X --- * --- N (newrev)
-        #
-        # That is to say, there is no guarantee that oldrev is a strict
-        # subset of newrev (it would have required a --force, but that's
-        # allowed).  So, we can't simply say rev-list oldrev..newrev.
-        # Instead we find the common base of the two revs and list from
-        # there.
-        #
-        # As above, we need to take into account the presence of X; if
-        # another branch is already in the repository and points at some of
-        # the revisions that we are about to output - we don't want them.
-        # The solution is as before: git rev-parse output filtered.
-        #
-        # Finally, tags: 1 --- 2 --- O --- T --- 3 --- 4 --- N
-        #
-        # Tags pushed into the repository generate nice shortlog emails that
-        # summarise the commits between them and the previous tag.  However,
-        # those emails don't include the full commit messages that we output
-        # for a branch update.  Therefore we still want to output revisions
-        # that have been output on a tag email.
-        #
-        # Luckily, git rev-parse includes just the tool.  Instead of using
-        # "--all" we use "--branches"; this has the added benefit that
-        # "remotes/" will be ignored as well.
-
         if self.new.commit and not self.old.commit:
             # A new reference was created.  List the new revisions
             # brought by the new reference (i.e., those revisions that
@@ -1498,7 +1427,62 @@ class GitoliteEnvironment(ConfigEnvironment):
 
 
 class Push(object):
-    """Represent an entire push (i.e., a group of ReferenceChanges)."""
+    """Represent an entire push (i.e., a group of ReferenceChanges).
+
+    It is easy to figure out what commits were added to a *branch* by
+    a Reference change:
+
+        git rev-list change.old..change.new
+
+    or removed from a *branch*:
+
+        git rev-list change.new..change.old
+
+    But it is not quite so trivial to determine which entirely new
+    commits were added to the *repository* by a push and which old
+    commits were discarded by a push.  A big part of the job of this
+    class is to figure out these things, and to make sure that new
+    commits are only detailed once even if they were added to multiple
+    references.
+
+    The first step is to determine the "other" references--those
+    unaffected by the current push.  They are computed by
+    Push._compute_other_refs() by listing all references then removing
+    any affected by this push.
+
+    The commits contained in the repository before this push were
+
+        git rev-list other1 other2 other3 ... change1.old change2.old ...
+
+    Where "changeN.old" is the old value of one of the references
+    affected by this push.
+
+    The commits contained in the repository after this push are
+
+        git rev-list other1 other2 other3 ... change1.new change2.new ...
+
+    The commits added by this push are the difference between these
+    two sets, which can be written
+
+        git rev-list \
+            ^other1 ^other2 ... \
+            ^change1.old ^change2.old ... \
+            change1.new change2.new ...
+
+    The commits removed by this push can be computed by
+
+        git rev-list \
+            ^other1 ^other2 ... \
+            ^change1.new ^change2.new ... \
+            change1.old change2.old ...
+
+    The last point is that it is possible that other pushes are
+    occurring simultaneously to this one, so reference values can
+    change at any time.  It is impossible to eliminate all race
+    conditions, but we reduce the window of time during which problems
+    can occur by translating reference names to SHA1s as soon as
+    possible and working with SHA1s thereafter (because SHA1s are
+    immutable)."""
 
     # A map {(changeclass, changetype) : integer} specifying the order
     # that reference changes will be processed if multiple reference
