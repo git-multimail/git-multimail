git-multimail/git-multimail

commit c145bb626c69f63876603dbd2045b040a2d2f770

Use repo_path from GenericEnvironment._compute_repo_shortname().

git-multimail/git_multimail.py
@@ -1761,15 +1761,7 @@ class GenericEnvironment(ConfigEnvironment):
             )
 
     def _compute_repo_shortname(self):
-        if read_git_output(['rev-parse', '--is-bare-repository']) == 'true':
-            path = GIT_DIR
-        else:
-            try:
-                path = read_git_output(['rev-parse', '--show-toplevel'])
-            except CommandError:
-                return 'unknown repository'
-
-        basename = os.path.basename(os.path.abspath(path))
+        basename = os.path.basename(os.path.abspath(self.repo_path))
         m = self.REPO_NAME_RE.match(basename)
         if m:
             return m.group('name')

commit 2f42ca1f5b74fc8eb41c492853cbcf880199c172

Fix an initialization order problem introduced by c145bb626c.
When GenericEnvironment._compute_repo_shortname() is called, the
repo_path member has not yet been initialized.  So call
self.compute_repo_path() rather than trying to refer to
self.repo_path.

git-multimail/git_multimail.py
@@ -1521,11 +1521,11 @@ class Environment(object):
         self.refchange_showlog = False
         self.reply_to_refchange = 'pusher'
         self.reply_to_commit = 'author'
-        self.repo_path = self.get_repo_path()
+        self.repo_path = self.compute_repo_path()
 
         self._values = None
 
-    def get_repo_path(self):
+    def compute_repo_path(self):
         if read_git_output(['rev-parse', '--is-bare-repository']) == 'true':
             path = GIT_DIR
         else:
@@ -1761,7 +1761,9 @@ class GenericEnvironment(ConfigEnvironment):
             )
 
     def _compute_repo_shortname(self):
-        basename = os.path.basename(os.path.abspath(self.repo_path))
+        # We have to call compute_repo_path() here because
+        # self.repo_path is not yet initialized:
+        basename = os.path.basename(os.path.abspath(self.compute_repo_path()))
         m = self.REPO_NAME_RE.match(basename)
         if m:
             return m.group('name')