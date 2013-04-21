29dcb3f0e936390eeabd4a5f5fc10886c9190f3e
Michael Haggerty <mhagger@alum.mit.edu>
2013-04-21 07:44:42 +0200

    Define and use named constants ENCODING and CHARSET.

--- git-multimail/git_multimail.py
+++ git-multimail/git_multimail.py
@@ -74,6 +74,13 @@ LOGBEGIN = '- Log --------------------------------------------------------------
 LOGEND = '-----------------------------------------------------------------------\n'
 
 
+# It is assumed in many places that the encoding is uniformly UTF-8,
+# so changing these constants is unsupported.  But define them here
+# anyway, to make it easier to find (at least most of) the places
+# where the encoding is important.
+(ENCODING, CHARSET) = ('UTF-8', 'utf-8')
+
+
 REF_CREATED_SUBJECT_TEMPLATE = (
     '%(emailprefix)s%(refname_type)s %(short_refname)s created'
     ' (now %(newrev_short)s)'
@@ -91,7 +98,7 @@ REFCHANGE_HEADER_TEMPLATE = """\
 To: %(recipients)s
 Subject: %(subject)s
 MIME-Version: 1.0
-Content-Type: text/plain; charset=utf-8
+Content-Type: text/plain; charset=%(charset)s
 Content-Transfer-Encoding: 8bit
 Message-ID: %(msgid)s
 From: %(fromaddr)s
@@ -217,7 +224,7 @@ REVISION_HEADER_TEMPLATE = """\
 To: %(recipients)s
 Subject: %(emailprefix)s%(num)02d/%(tot)02d: %(oneline)s
 MIME-Version: 1.0
-Content-Type: text/plain; charset=utf-8
+Content-Type: text/plain; charset=%(charset)s
 Content-Transfer-Encoding: 8bit
 From: %(fromaddr)s
 Reply-To: %(reply_to)s
@@ -577,7 +584,7 @@ class Change(object):
                 try:
                     h = Header(value, header_name=name)
                 except UnicodeDecodeError:
-                    h = Header(value, header_name=name, charset='utf-8', errors='replace')
+                    h = Header(value, header_name=name, charset=CHARSET, errors='replace')
                 for splitline in ('%s: %s\n' % (name, h.encode(),)).splitlines(True):
                     yield splitline
 
@@ -1502,6 +1509,7 @@ class Environment(object):
         'pusher_email',
         'fromaddr',
         'repo_path',
+        'charset',
         ]
 
     def __init__(self):
@@ -1526,6 +1534,7 @@ class Environment(object):
         self.reply_to_refchange = 'pusher'
         self.reply_to_commit = 'author'
         self.repo_path = self.get_repo_path()
+        self.charset = CHARSET
 
         self._values = None
 
@@ -1595,12 +1604,12 @@ class Environment(object):
         self.strict_utf8 as described above."""
 
         if self.strict_utf8:
-            lines = (line.decode('utf-8', 'replace') for line in lines)
+            lines = (line.decode(ENCODING, 'replace') for line in lines)
             # Limit the line length in Unicode-space to avoid
             # splitting characters:
             if self.maxlinelength:
                 lines = limit_linelength(lines, self.maxlinelength)
-            lines = (line.encode('utf-8', 'replace') for line in lines)
+            lines = (line.encode(ENCODING, 'replace') for line in lines)
         elif self.maxlinelength:
             lines = limit_linelength(lines, self.maxlinelength)
 
