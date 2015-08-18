7143b5f8e4b51595cfbac56f8d343e8b726d2933
Matthieu Moy <Matthieu.Moy@imag.fr>
2015-08-18 13:45:16 +0200

    t/generate-test-emails: move code to helper-functions.sh
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>

--- t/helper-functions.sh
+++ t/helper-functions.sh
@@ -23,3 +23,92 @@ if [ -z "$SHARNESS_TEST_DIRECTORY" ]
 then
     fatal "Please, source sharness.sh before helper-functions.sh"
 fi
+
+ZEROS=0000000000000000000000000000000000000000
+
+# Calling git-multimail
+MULTIMAIL="$SHARNESS_TEST_DIRECTORY/../git-multimail/git_multimail.py"
+MULTIMAIL_VERSION_QUOTED=$("$MULTIMAIL" --version |
+    sed -e 's/^git-multimail version //' -e 's@[/\\]@\\\0@g')
+POST_RECEIVE="$SHARNESS_TEST_DIRECTORY/../git-multimail/post-receive.example"
+
+
+test_email() {
+    REFNAME="$1"
+    OLDREV="$2"
+    NEWREV="$3"
+    shift 3
+    pecho "$OLDREV" "$NEWREV" "$REFNAME" | USER=pushuser "$MULTIMAIL" "$@"
+}
+
+test_create() {
+    REFNAME="$1"
+    NEWREV=$(git rev-parse "$REFNAME")
+    shift
+    test_email "$REFNAME" "$ZEROS" "$NEWREV" "$@"
+}
+
+test_update() {
+    REFNAME="$1"
+    OLDREV=$(git rev-parse "$2")
+    NEWREV=$(git rev-parse "$REFNAME")
+    shift 2
+    test_email "$REFNAME" "$OLDREV" "$NEWREV" "$@"
+}
+
+test_delete() {
+    REFNAME="$1"
+    OLDREV=$(git rev-parse "$REFNAME")
+    shift
+    git update-ref -d "$REFNAME" "$OLDREV" &&
+    test_email "$REFNAME" "$OLDREV" "$ZEROS" "$@"
+    RETCODE=$?
+    git update-ref "$REFNAME" "$OLDREV" ||
+        error "Error replacing reference $REFNAME to $OLDREV"
+    return $RETCODE
+}
+
+test_rewind() {
+    REFNAME="$1"
+    OLDREV=$(git rev-parse "$REFNAME")
+    NEWREV=$(git rev-parse "$2")
+    shift 2
+    git update-ref "$REFNAME" "$NEWREV" "$OLDREV" &&
+    test_email "$REFNAME" "$OLDREV" "$NEWREV" "$@"
+    RETCODE=$?
+    git update-ref "$REFNAME" "$OLDREV" ||
+        error "Error replacing reference $REFNAME to $OLDREV"
+    return $RETCODE
+}
+
+# Like test_update, but using example post-receive script:
+test_hook() {
+    REFNAME="$1"
+    OLDREV=$(git rev-parse "$2")
+    NEWREV=$(git rev-parse "$REFNAME")
+    shift 2
+    pecho "$OLDREV" "$NEWREV" "$REFNAME" | USER=pushuser "$POST_RECEIVE" "$@"
+}
+
+verbose_do() {
+    if test $# -gt 1
+    then
+	(
+	    printf "\$ %s" "$1"
+	    shift
+	    # Show each argument quoted (e.g. to distinguish between
+	    # '' and nothing at all).
+	    printf " '%s'" "$@"
+	    printf '\n'
+	)
+    else
+	printf "\$ %s\n" "$*"
+    fi
+    "$@"
+}
+
+# Default configuration
+HOME="$SHARNESS_TEST_DIRECTORY"
+XDG_CONFIG_HOME="$SHARNESS_TEST_DIRECTORY"
+GIT_CONFIG_NOSYSTEM=1
+export HOME XDG_CONFIG_HOME GIT_CONFIG_NOSYSTEM
