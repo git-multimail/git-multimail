f1c84bf03a1cabda1ddf2080c115dca9af374e20
Matthieu Moy <Matthieu.Moy@imag.fr>
2015-08-31 17:54:46 +0200

    python3 compatibility: use list(<dict>.keys())
    In Python 2, <dict>.keys() returned a list. In Python 3, it returns a
    dict_keys that needs to be converted into a list.
    
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>

--- git-multimail/git_multimail.py
+++ git-multimail/git_multimail.py
@@ -3523,7 +3523,7 @@ def main(args):
 
     parser.add_option(
         '--environment', '--env', action='store', type='choice',
-        choices=KNOWN_ENVIRONMENTS.keys(), default=None,
+        choices=list(KNOWN_ENVIRONMENTS.keys()), default=None,
         help=(
             'Choose type of environment is in use.  Default is taken from '
             'multimailhook.environment if set; otherwise "generic".'
