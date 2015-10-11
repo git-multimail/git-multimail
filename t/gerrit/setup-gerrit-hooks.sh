bf208e39753934a06f83dd65b89dc81112fb923e
Matthieu Moy <Matthieu.Moy@imag.fr>
2015-10-11 19:45:42 +0200

    gerrit: restart server after changing configuration
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>

--- t/gerrit/setup-gerrit-hooks.sh
+++ t/gerrit/setup-gerrit-hooks.sh
@@ -9,3 +9,6 @@ file=/opt/gerrit/etc/gerrit.config
 git config --file "$file" hooks.path $(pwd)
 git config --file "$file" hooks.refUpdatedHook git-multimail-wrapper.sh
 # git config --file "$file" hooks.refUpdateHook git-multimail-wrapper.sh
+
+# Gerrit needs to be restarted to re-read the config file
+/opt/gerrit/bin/gerrit.sh restart
