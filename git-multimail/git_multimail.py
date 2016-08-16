6dd0a159d5123467f5923d5d58478f1b492f8c0b
Matthieu Moy <Matthieu.Moy@imag.fr>
2016-08-16 19:58:29 +0200

    Alternate implementation of p.terminate for Python 2.4
    Ignoring the exception was interacting with the 'raise' statement right
    after, which was raising the wrong exception.
    
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>

--- git-multimail/git_multimail.py
+++ git-multimail/git_multimail.py
@@ -2010,11 +2010,12 @@ class SendMailer(Mailer):
                 '*** Error while generating commit email\n'
                 '***  - mail sending aborted.\n'
                 )
-            try:
+            if hasattr(p, 'terminate'):
                 # subprocess.terminate() is not available in Python 2.4
                 p.terminate()
-            except AttributeError:
-                pass
+            else:
+                import signal
+                os.kill(p.pid, signal.SIGTERM)
             raise
         else:
             p.stdin.close()
